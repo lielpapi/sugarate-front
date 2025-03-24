35016ee6dfe6de7220fdb571c9b22bb78c1f22f7
ophir <segalophir@gmail.com>
2025-03-24 17:11:42 +0200

    final

--- src/services/postService.ts
+++ src/services/postService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import authService from './authService';
 
-const API_URL = 'http://localhost:5173/api/posts';
+const API_URL = 'http://localhost:5000/api/posts';
 
 export interface HealthMetrics {
   location?: string;
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:5173/api';
+const API_BASE_URL = 'http://localhost:5000/api';
 
 interface UserData {
   username: string;
